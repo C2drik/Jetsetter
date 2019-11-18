55689652fd541a23e4736f25bc41c60c34e082be
Cedrik Razafimanantsoa <cedrik.razafi@gmail.com>
2019-11-18 16:53:06 +0100

    Trip model saved

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,8 +4,7 @@ class Trip < ApplicationRecord
   validates :start_date, presence: true
   validates :start_city, presence: true
   validates :end_city, presence: true
-  validates :jet_id, presence: true
+  validates :jet_name, presence: true
   validates :price, presence: true
-  validates :user_id, presence: true, uniqueness: true
 
 end
