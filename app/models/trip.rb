e23eb00cb7e58b7901dbe593762c6f6100c4f341
Cedrik Razafimanantsoa <cedrik.razafi@gmail.com>
2019-11-18 16:49:57 +0100

    Booking & Trip Models generated

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,11 @@
 class Trip < ApplicationRecord
   belongs_to :user
+
+  validates :start_date, presence: true
+  validates :start_city, presence: true
+  validates :end_city, presence: true
+  validates :jet_id, presence: true
+  validates :price, presence: true
+  validates :user_id, presence: true, uniqueness: true
+
 end
