3c465c482147ee7fe122c09dd96425972e6fe57b
ArafathNguyen <nguyen.arafath@gmail.com>
2019-11-19 17:31:19 +0100

    AddCreateToBookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,5 @@
 class BookingsController < ApplicationController
 
-  def dashboard
-    # create dashboard
-    booking = Booking.where(id: current_user.id)
-  end
 
   def new
     @trip = Trip.find(params[:trip_id])
@@ -11,6 +7,15 @@ class BookingsController < ApplicationController
   end
 
   def create
-
+    @trip = Trip.find(params[:trip_id])
+    @booking = Booking.new
+    @booking.user = current_user
+    @booking.trip = @trip
+    if @booking.save
+      redirect_to trip_path(@trip)
+    else
+      render "trips/show"
+    end
   end
+
 end
