d6e75ea2fb6adbccf6a469d4e1c5a7deb58f1086
ArafathNguyen <nguyen.arafath@gmail.com>
2019-11-19 11:24:19 +0100

    tripscontroller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,9 +1,32 @@
 class TripsController < ApplicationController
+before_action :set_trip, only: [:show, :edit, :update]
+
   def index
     @trip = Trip.all
   end
 
   def show
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip.save
+  end
+
+  def edit
+  end
+
+  def update
+    @trip.update(params[:trip])
+  end
+
+  def destroy
+  end
+
+  def set_trip
     @trip = Trip.find(params[:id])
   end
 end
