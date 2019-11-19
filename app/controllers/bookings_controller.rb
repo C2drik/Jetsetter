752b12a762c15a8e6520956bf9bf88dc72561060
Etienne Testard <etitestard@gmail.Com>
2019-11-19 12:45:02 +0100

    Dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,38 +1,6 @@
 class BookingsController < ApplicationController
 
-  def index
-    @booking = Booking.all
+  def dashboard
+    booking = Booking.where(id: current_user.id)
   end
-
-  def show
-    @booking = Booking.find(params[:id])
-  end
-
-  def new
-    @booking = Booking.new
-  end
-
-  def create
-    @booking = Booking.new(params[:booking])
-    @booking.save
-  end
-
-  def edit
-    @booking = Booking.find(params[:id])
-  end
-
-  def update
-    if @booking.update(params[:id])
-      render :show
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def booking_params
-    params.require(:booking).permit(:status)
-  end
-
 end
