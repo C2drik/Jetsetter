b46f60a571219b560cafb470e7e7c870867b5568
anniou toybou <anniou.toybou@gmail.com>
2019-11-21 13:57:44 +0100

    Add new style show

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
        resources :bookings, only: [:new, :create, :update]
     end
     resources :bookings, only: [:index, :show]
+
     get '/dashboard', to: 'dashboard#show'
   end
